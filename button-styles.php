bb2435eeb9f0457382c62a476304f2c4574d78bd
Matthew Ciampa <webbedmedia@gmail.com>
2015-12-21 20:16:57 -0500

    created stub page

---
+++ button-styles.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Button Styles
+*/
+get_header(); ?>
+
+<div class="container">
+	<div class="pad">
+		<a href="#" class="btn btn-default">click me</a>
+	</div>
+	<div class="pad">
+		<a href="#" class="btn btn-primary">click me</a>
+	</div>
+	<div class="pad">
+		<a href="#" class="btn btn-warning">click me</a>
+	</div>
+	<div class="pad">
+		<a href="#" class="btn btn-danger">click me</a>
+	</div>
+	<div class="pad">
+		<a href="#" class="btn btn-success">click me</a>
+	</div>
+</div>
+
+<?php get_footer(); ?>
